49b235fb8947edbf7c906143ebdb18b0c93168bf
Paulo Moreno <106778522+PauloMorenoD@users.noreply.github.com>
2023-07-28 17:29:45 -0300

    feat : add initial user auth

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,16 +3,32 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index()
     {
-        $name = "Paulo";
-        return view('login', ['name' => $name]);
+        return view('login');
     }
+    public function auth(Request $request){
+        echo $request;
+        return redirect('home');
+    }
+
     public function register()
     {
         return view('register');
+    }
+    public function create(Request $request)
+    {
+        $user = new User;
+
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+
+        $user->save();
+        return redirect('/')->with('msg', 'Usuário criado com sucesso');
     }
 }
